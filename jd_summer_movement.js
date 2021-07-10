461cfea1f5efbb98717415938098eaf0db9ad7a8
qq1270735037 <74816245+qq1270735037@users.noreply.github.com>
2021-07-10 23:45:31 +0800

    Update jd_summer_movement.js

--- jd_summer_movement.js
+++ jd_summer_movement.js
@@ -9,7 +9,7 @@ https://raw.githubusercontent.com/smiek2221/scripts/master/MovementFaker.js
 
 */
 const $ = new Env('燃动夏季');
-const MovementFaker = require('./qq1270735037_daguaishou_MovementFaker.js')
+const MovementFaker = require('./qq1270735037_daguaishou_jd_MovementFaker.js')
 // const MovementFaker = require('./ZooFaker.js')
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
