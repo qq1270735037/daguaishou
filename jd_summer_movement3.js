822ffbc371a796aeab97e7a1a0beddb596e9ca3c
qq1270735037 <74816245+qq1270735037@users.noreply.github.com>
2021-07-26 02:23:53 +0800

    Update jd_summer_movement3.js

--- jd_summer_movement3.js
+++ jd_summer_movement3.js
@@ -20,7 +20,7 @@ if ($.isNode() && process.env.summer_movement_joinjoinjoinhui) {
 }
 
 // 百元守卫战SH
-let summer_movement_ShHelpFlag = 1;// 0不开启也不助力 1开启并助力 2开启但不助力
+let summer_movement_ShHelpFlag =0;// 0不开启也不助力 1开启并助力 2开启但不助力
 if ($.isNode() && process.env.summer_movement_ShHelpFlag) {
   summer_movement_ShHelpFlag = process.env.summer_movement_ShHelpFlag;
 }
